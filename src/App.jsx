0e927673e6775c6c1e51ccfe24998020c6434eb7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-31 17:59:19 -0600

    Implement Basic Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 //Libraries
-import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 // Components
 import NavigationBar from './components/NavigationBar';
-import { HomePage } from './components/HomePage';
-import { DetailsPage } from './components/DetailsPage';
+import  HomePage  from './components/HomePage';
+import DetailsPage from './components/DetailsPage';
 
 function App() {
   return (
     <>
       <NavigationBar />
-      <Routes>
+      <Routes >
         <Route path='/' element={<HomePage/>} />
         <Route path='/home' element={<HomePage/>} />
         <Route path='/details' element={<DetailsPage/>} />
-      </Routes>
+      </Routes >
     </>
   );
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,17 +1,39 @@
-import '../styling/NavigationBar.css'
-import { AiTwotoneSetting } from "react-icons/ai"
+import '../styling/NavigationBar.css';
+import { AiTwotoneSetting } from 'react-icons/ai';
+import { NavLink } from 'react-router-dom';
 
-
-const NavigationBar = ()=>{
-    return (
-        <nav>
-            <ul>
-                <li><button>{"<"}</button> <span>2015</span></li>
-                <li>most views</li>
-                <li><button>< AiTwotoneSetting /></button></li>
-            </ul>
-        </nav>
-    )
-}
+const NavigationBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <button>{'<'}</button> <span>2015</span>
+        </li>
+        <li>most views</li>
+        <li>
+          <NavLink
+            to="/home"
+            className={({isActive}) => (isActive ? 'active-link' : '')}
+          >
+            TestHomePage
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/details"
+            className={({isActive}) => (isActive ? 'active-link' : '')}
+          >
+            TestDetailsPage
+          </NavLink>
+        </li>
+        <li>
+          <button>
+            <AiTwotoneSetting />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavigationBar;
