cfff44dd03a131f8645b82b8a46695a39a2d23dc
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-31 16:01:23 -0600

    Implements Initial routs, Removes un-necesary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Libraries
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+// Components
+import NavigationBar from './components/NavigationBar';
+import { HomePage } from './components/HomePage';
+import { DetailsPage } from './components/DetailsPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavigationBar />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/home' element={<HomePage/>} />
+        <Route path='/details' element={<DetailsPage/>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
