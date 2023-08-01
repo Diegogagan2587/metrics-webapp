effbf59a0a58974ba530c7af949c9724a6101543
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-01 15:20:49 -0600

    Refactor fetchWeather.js to accept latitude and longitude as arguments

--- src/redux/home/fetchWeather.js
+++ src/redux/home/fetchWeather.js
@@ -1,12 +1,11 @@
-const keyAPI = 'c33def0700f3ac14afe2ffead5486742';
-const latitud = '17';
-const longitud = '92';
-const endPoint = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitud}&lon=${longitud}&appid=${keyAPI}`;
 
-const fetchWeather = async () => {
+const keyAPI = 'c33def0700f3ac14afe2ffead5486742';
+
+
+const fetchWeather = async ({latitude,longitude}) => {
+const endPoint = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=${keyAPI}`;
   const request = await fetch(endPoint);
   const response = await request.json();
-  console.log('response---->', response);
   return response;
 };
 
