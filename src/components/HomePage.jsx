34123e1372b1b5c112752a3ca5d07db399a5347c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-01 18:28:47 -0600

    Import useDispatch, dispatch updateSelected reducer

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,18 @@
 import '../styling/HomePage.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 const continentsURL =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Continental_models-Australia.gif/1280px-Continental_models-Australia.gif';
 
 const HomePage = () => {
+  const dispatch = useDispatch();
   const statesInCountry = useSelector((state) => state.weather.mexico.states);
+
+  const eventHandler = (event) => {
+    console.log(event.target);
+    dispatch(updateSelected());
+  };
 
   return (
     <>
@@ -24,10 +30,10 @@ const HomePage = () => {
             const { latitude, longitude } = state;
             return (
               <li key={state.state} className="country">
-                <NavLink to="/details">
+                <NavLink to="/details" onClick={eventHandler}>
                   <h3>{state.state}</h3>
-                  <span>{`coordinates: Lat ${latitude}, Long ${longitude}`}</span>
                 </NavLink>
+                <span>{`coordinates: Lat ${latitude}, Long ${longitude}`}</span>
               </li>
             );
           })}
