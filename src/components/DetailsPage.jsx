5d3f181071bd2e69ab477ef1c4c6b073c7a29fe2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-02 20:33:26 -0600

    Import styling and rename classes

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,3 +1,4 @@
+import '../styling/DetailsPage.css'
 import { useSelector } from 'react-redux';
 
 const DetailsPage = () => {
@@ -5,28 +6,28 @@ const DetailsPage = () => {
   const currentCity = useSelector((state)=>state.details[0].selected);
   const currentCityData = cities.filter((city)=>city.state===`${currentCity}`);
   const { state:name, img} = currentCityData[0];
-  console.log('current city in details--->',currentCityData[0])
+  console.log('detailsPage is receiving---->', name)
   const {components:polutionStats} = currentCityData[0].data[0];
   return (
-    <div>
-      <section className="continent">
-        <img src={img} alt="continents picture" />
-        <div className="cont-header">
+    <div className='details-section'>
+      <section className="state">
+        <img src={img} alt="state picture" />
+        <div className="state-header">
           <h1>{name}</h1>
           <span>6958 views</span>
         </div>
       </section>
-      <section>
-        <h2>Polution Data</h2>
-        <ul>
-            <li><span>CO:</span> <span>{polutionStats.co}</span> </li>
-            <li><span>nh3:</span> <span>{polutionStats.nh3}</span> </li>
-            <li><span>no:</span> <span>{polutionStats.no}</span> </li>
-            <li><span>no2:</span> <span>{polutionStats.no2}</span> </li>
-            <li><span>o3:</span> <span>{polutionStats.o3}</span> </li>
-            <li><span>pm2_5:</span> <span>{polutionStats.pm2_5}</span> </li>
-            <li><span>pm10:</span> <span>{polutionStats.pm10}</span> </li>
-            <li><span>so2:</span> <span>{polutionStats.so2}</span> </li>
+      <section className='data'>
+        <h2 className='state-h2'>State Breackdown</h2>
+        <ul className='data-list'>
+            <li><span className='bold'>CO:</span> <span>{polutionStats.co}</span> </li>
+            <li><span className='bold'>NH3:</span> <span>{polutionStats.nh3}</span> </li>
+            <li><span className='bold'>NO:</span> <span>{polutionStats.no}</span> </li>
+            <li><span className='bold'>NO2:</span> <span>{polutionStats.no2}</span> </li>
+            <li><span className='bold'>O3:</span> <span>{polutionStats.o3}</span> </li>
+            <li><span className='bold'>PM2_5:</span> <span>{polutionStats.pm2_5}</span> </li>
+            <li><span className='bold'>PM10:</span> <span>{polutionStats.pm10}</span> </li>
+            <li><span className='bold'>SO2:</span> <span>{polutionStats.so2}</span> </li>
         </ul>
       </section>
     </div>
