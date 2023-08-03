8f4895a534220f07f9cdf6a8d986c441d1abb3c5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-03 15:41:26 -0600

    Import Navigation slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weatherReducer from './home/homeSlice';
 import detailsReducer from './details/detailsSlice';
+import navigationReducer from './navigation/navigationSlice';
 
 const store = configureStore({
   reducer: {
     weather: weatherReducer,
     details: detailsReducer,
+    navigaton: navigationReducer,
   },
 });
 
