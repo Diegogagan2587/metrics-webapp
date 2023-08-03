5921e274cd42a413dbe842860c62911ede92e6e6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-03 13:37:20 -0600

    Add basename for <BroserRouter> to /metrics-webapp

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/metrics-webapp'>
       <Provider store={store}>
         <App />
       </Provider>
