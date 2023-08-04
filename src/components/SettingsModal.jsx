47089fcfdc409ece7b74ee0905a2c94c46ea9f6a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-03 21:15:25 -0600

    Import and dispatch reducer to update the number of cities to be displayed within the home page

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -1,15 +1,17 @@
 import '../styling/SettingsModal.css'
 import {useSelector, useDispatch } from 'react-redux';
-import { closeModal } from '../redux/navigation/navigationSlice';
+import { closeModal, updateNumberOfCities } from '../redux/navigation/navigationSlice';
 const SettingsModal = () => {
     const dispatch = useDispatch();
-    const isModalOPen = useSelector((state)=>state.navigaton[0].isModalOpen)
+    const isModalOPen = useSelector((state)=>state.navigaton[0].isModalOpen);
     const setCloseModal = ()=>{
         dispatch(closeModal())
     }
     const handleSubmit = (event)=>{
         event.preventDefault()
-        console.log(event.target)
+        const numberFromUser = event.target.querySelector('.modal-input').value;
+        console.log(numberFromUser);
+        dispatch(updateNumberOfCities(numberFromUser))
     }
     return (
         <div className="modal-background hide">
@@ -17,7 +19,7 @@ const SettingsModal = () => {
             <button className="close-mod" onClick={()=>setCloseModal()}>X</button>
             <form onSubmit={(event)=>handleSubmit(event)}>
                 <p>How many states do you want to display?</p>
-                <input type="text" name="modal-input" id="modal-input" />
+                <input type="text" name="modal-input" id="modal-input" className='modal-input'/>
                 <button type="submit">Change</button>
             </form>
             </div>
