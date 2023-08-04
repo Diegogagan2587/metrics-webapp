06d2b5238b4cb08b863b404e0eecfaa292330750
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-03 21:52:50 -0600

    Fix Error: Fetching data in a loop

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       const { latitude, longitude } = state;
       dispatch(getWeatherAsync({ latitude, longitude }));
     });
-  }, [dispatch,statesInCountry]);
+  }, []);
 
   return (
     <>
