d98fd78ef0c1370704d50bed81ab311772a8e959
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-05 15:31:04 -0600

    Fix linters

--- src/components/translateAirQualityFrom.js
+++ src/components/translateAirQualityFrom.js
@@ -3,19 +3,14 @@ const translateAirQualityFrom = (number) => {
     switch (number) {
       case 1:
         return 'Good';
-        break;
       case 2:
         return 'Fair';
-        break;
       case 3:
         return 'Moderate';
-        break;
       case 4:
         return 'Poor';
-        break;
       case 5:
         return 'Very Poor';
-        break;
     }
   };
 
