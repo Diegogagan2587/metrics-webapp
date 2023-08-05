17d16c01103684bc594e48eb0e827d83477cd84e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-04 19:20:33 -0600

    Add first test for HomePage componenet

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
-import DetailsPage from '../components/DetailsPage';
+import { Provider } from "react-redux";
+// import DetailsPage from '../components/DetailsPage';
 import HomePage from '../components/HomePage';
-import SettingsModal from '../components/SettingsModal';
-
+// import SettingsModal from '../components/SettingsModal';
+import store from '../redux/store';
+jest.mock('../assets/mapMexico.png');
 describe('Test Components',()=> {
     describe('test DetailsPage Component', ()=>{
         test("this is just a test for testing enviroment",()=>{
             expect(2+2).toBe(4);
+        }),
+        test('HomePage should render Mexico',()=>{
+            render(
+                <BrowserRouter><Provider store={store}><HomePage /></Provider></BrowserRouter>
+            )
+            const mexico = screen.getByText('Mexico')
+            expect(mexico).toBeInTheDocument();
         })
+        
     })
 })
