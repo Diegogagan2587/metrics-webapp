099b47d0eac5e91e417858968f911b01f168f70c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-07 20:18:39 -0600

    Add Default case for function translateAirQualityFrom()

--- src/components/translateAirQualityFrom.js
+++ src/components/translateAirQualityFrom.js
@@ -1,17 +1,19 @@
 const translateAirQualityFrom = (number) => {
-  if(!number) return  'notFound';
-    switch (number) {
-      case 1:
-        return 'Good';
-      case 2:
-        return 'Fair';
-      case 3:
-        return 'Moderate';
-      case 4:
-        return 'Poor';
-      case 5:
-        return 'Very Poor';
-    }
-  };
+  if (!number) return 'notFound';
+  switch (number) {
+    case 1:
+      return 'Good';
+    case 2:
+      return 'Fair';
+    case 3:
+      return 'Moderate';
+    case 4:
+      return 'Poor';
+    case 5:
+      return 'Very Poor';
+    default:
+      return 'Not founded';
+  }
+};
 
-  export default translateAirQualityFrom;
+export default translateAirQualityFrom;
