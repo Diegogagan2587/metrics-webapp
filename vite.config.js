2082c4d859ce737f938b73fbd45f7ded8bf4f00d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-07 20:01:01 -0600

    instal vite-plugin-eslint to fully integrate eslint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:'/metrics-webapp/',
-})
+  plugins: [react(), eslint()],
+  base: '/metrics-webapp/',
+});
