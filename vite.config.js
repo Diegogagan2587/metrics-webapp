28e4242a3ef8f18b0aa634166340164f8be279f6
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-08-07 20:12:13 -0600

    Disable Eslint for vite.config.js since it is a development tool, not project dependencie

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+//Disable Eslint since vite is a development tool
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
